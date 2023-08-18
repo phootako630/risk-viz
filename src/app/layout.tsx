59623a1030f10dec92a97bd8ccf916524090bc45
ivanzhang <ivanzhang220820@gmail.com>
2023-08-18 14:13:36 -0400

    Suspense fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,34 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Loading from "./loading";
-import { Suspense } from "react";
+import React, { Suspense } from "react";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Climate Risk Dashboard",
-  description:
-      "Assessing the impact of climate change on Canadian businesses throughout the 21st century. As global temperatures rise and extreme weather events become more frequent, businesses across various industries are facing increasing risks and challenges. This analysis aims to provide insights into the potential consequences of climate change, empowering businesses to make informed decisions, adapt to changing conditions, and contribute to a sustainable future.",
-  imageUrl: "/background.png",
+    title: "Climate Risk Dashboard",
+    description: "Assessing the impact of climate change on Canadian businesses...",
+    imageUrl: "/background.png",
 };
 
-<meta property="og:image" content={metadata.imageUrl} />;
-<meta name="twitter:image" content={metadata.imageUrl} />;
-
 export default function RootLayout({
-                                     children,
+                                       children,
                                    }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-      <html lang="en">
-      <>
+    return (
+        <html lang="en">
+        <Head>
+            <meta property="og:image" content={metadata.imageUrl} />
+            <meta name="twitter:image" content={metadata.imageUrl} />
+            <title>{metadata.title}</title>
+        </Head>
+        <body className={inter.className}>
         <Suspense fallback={<Loading />}>
-          <body className={inter.className}>{children}</body>
+            {children}
         </Suspense>
-      </>
-      </html>
-  );
+        </body>
+        </html>
+    );
 }
